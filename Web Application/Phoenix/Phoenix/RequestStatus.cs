e9e77f4cfbff0f2d1da2cd635c438ad8dfde62dd
yang.xue <yang.xue@accenture.com>
2017-12-15 17:14:30 +0800

    add file of RequestStatus.cs

---
+++ Web Application/Phoenix/Phoenix/RequestStatus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Phoenix
+{
+    public class RequestStatus
+    {
+        public enum RequestStatusDetail
+        {
+            PENDINGREVIEW = 0,
+            APPROVED = 1,
+            REJECTED = 2,
+            COMPLETED = 3
+
+        }
+    }
+}
