2728fd515fd43fa85615f0631b49c4d55b070c38
Yohanna12138 <yuhan.wang@accenture.com>
2017-12-24 11:21:07 +0800

    test

--- Phoenixw/Phoenixw/PhoenixRESTService.svc.cs
+++ Phoenixw/Phoenixw/PhoenixRESTService.svc.cs
@@ -1,15 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
-using System.Data;
 using System.Data.SqlClient;
-using Newtonsoft.Json;
 using System.ServiceModel.Web;
 using System.ServiceModel.Activation;
-using System.Threading.Tasks;
 
 namespace Phoenixw
 {
@@ -20,9 +13,9 @@ namespace Phoenixw
     public class PhoenixRESTService : IPhoenixRESTService
     {
         //连接本地数据库
-        //SqlConnection sqlConnection = new SqlConnection(@"server=.\SQL2014;database=Phoenix;integrated security=sspi");
-        
-        SqlConnection sqlConnection = new SqlConnection(@"Server=tcp:mhedb.database.chinacloudapi.cn,1433;Initial Catalog = zengguosqlservertest; Persist Security Info=False;uid=mheadmin;password=Dalian@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
+        SqlConnection sqlConnection = new SqlConnection(@"server=.\SQL2014;database=Phoenix;integrated security=sspi");
+        //Azure数据库
+        //SqlConnection sqlConnection = new SqlConnection(@"Server=tcp:mhedb.database.chinacloudapi.cn,1433;Initial Catalog = zengguosqlservertest; Persist Security Info=False;uid=mheadmin;password=Dalian@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout = 30;");
 
         public void InsertRequest(PhoenixRequest phoenixRequest)
         {
@@ -41,9 +34,8 @@ namespace Phoenixw
                     var editDttm = DateTime.Now;
                     UpdateRequestToDB(requestId, comments, requestStatus, editDttm);
                 }
-
-                woc.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
              }
+
             catch (Exception ex)
             {
                 woc.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.ExpectationFailed;
@@ -52,54 +44,61 @@ namespace Phoenixw
 
         }
 
-        //添加数据
+        //更新数据库
         private void UpdateRequestToDB(string requestId, string comments, Status requestStatus, DateTime editDttm)
         {
-            //try
-            //{
-            //    sqlConnection.Open();
-            //    //string strSql = "SELECT RequestId FROM Request";
-            //    //DataSet ds = new DataSet();
-            //    //SqlDataAdapter s = new SqlDataAdapter(strSql, sqlConnection);
-            //    //s.Fill(ds);
-            //    SqlCommand mycmd = new SqlCommand("SELECT RequestId FROM Request", sqlConnection);
-            //    SqlDataReader mysdr = mycmd.ExecuteReader();
-            //    if (mysdr.HasRows)
-            //    {
-            //        //已经有记录使用此编号
-            //    }
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    throw ex;
-            //}
-            //finally
-            //{
-            //    sqlConnection.Close();
-            //}
+            WebOperationContext woc = WebOperationContext.Current;
             try
             {
                 sqlConnection.Open();
+                SqlCommand mycmd = new SqlCommand("SELECT RequestId FROM Request WHERE RequestId=@requestId", sqlConnection);
+                SqlParameter parq = new SqlParameter("@requestId", requestId);
+                mycmd.Parameters.Add(parq);
+                SqlDataReader mysdr = mycmd.ExecuteReader();
+                if (mysdr.HasRows)
+                {
+                    sqlConnection.Close();
+                    //已经有记录使用此编号
+                    try
+                    {
+                        sqlConnection.Open();
 
-                string strSql = "UPDATE  Request  SET Comments=@comments,RequestStatus=@requestStatus,EditDttm=@editDttm WHERE RequestId=@requestId";
-                SqlCommand cmd = new SqlCommand(strSql, sqlConnection);
-                SqlParameter parn = new SqlParameter("@requestId", requestId);
-                cmd.Parameters.Add(parn);
-                SqlParameter parc = new SqlParameter("@comments", comments);
-                cmd.Parameters.Add(parc);
-                SqlParameter parre = new SqlParameter("@requestStatus", requestStatus);
-                cmd.Parameters.Add(parre);
-                SqlParameter parra = new SqlParameter("@editDttm", editDttm);
-                cmd.Parameters.Add(parra);
-                //result接受受影响的行数，也就是说大于0的话表示添加成功
-                int result = cmd.ExecuteNonQuery();
-                cmd.Dispose();
+                        string strSql = "UPDATE  Request  SET Comments=@comments,RequestStatus=@requestStatus,EditDttm=@editDttm WHERE RequestId=@requestId";
+                        SqlCommand cmd = new SqlCommand(strSql, sqlConnection);
+                        SqlParameter parn = new SqlParameter("@requestId", requestId);
+                        cmd.Parameters.Add(parn);
+                        SqlParameter parc = new SqlParameter("@comments", comments);
+                        cmd.Parameters.Add(parc);
+                        SqlParameter parre = new SqlParameter("@requestStatus", requestStatus);
+                        cmd.Parameters.Add(parre);
+                        SqlParameter parra = new SqlParameter("@editDttm", editDttm);
+                        cmd.Parameters.Add(parra);
+                        //result接受受影响的行数，也就是说大于0的话表示添加成功
+                        int result = cmd.ExecuteNonQuery();
+                        cmd.Dispose();
+
+                        woc.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                        woc.OutgoingResponse.StatusDescription = "Update successfully!";
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+                else
+                {
+                    woc.OutgoingResponse.StatusCode = System.Net.HttpStatusCode.OK;
+                    woc.OutgoingResponse.StatusDescription = "Can not find the requestId";
+
+                }
             }
+
             catch (Exception ex)
             {
                 throw ex;
             }
+
             finally
             {
                 sqlConnection.Close();
@@ -107,6 +106,4 @@ namespace Phoenixw
         }
 
     }
-
-
 }
