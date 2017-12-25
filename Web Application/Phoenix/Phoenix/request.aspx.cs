e8568691cfab706f2618aa198b2439e9c5ed59fa
EmmaCat <li.f.liu@accentue.com>
2017-12-25 16:54:48 +0800

    test

--- Web Application/Phoenix/Phoenix/request.aspx.cs
+++ Web Application/Phoenix/Phoenix/request.aspx.cs
@@ -2,6 +2,7 @@
 using System.Data.SqlClient;
 using System;
 using System.Text;
+using Phoenix.PhoenixDataModel;
 
 namespace Phoenix
 {
@@ -13,56 +14,62 @@ namespace Phoenix
             RequestId.Text = requestid;
         }
 
-        private readonly SqlConnection _sqlConnection = new SqlConnection(@"server=.\SQL2014;database=Phoenix;integrated security=sspi");
-        private const string InsertRequest = "insert into Request(RequestId,RequestTitle,RequestDetail,Comments,RequestStatus,EditDttm) values('{0}','{1}','{2}','',{3},'{4}')";
+        //private readonly SqlConnection _sqlConnection = new SqlConnection(@"server=.\SQL2014;database=Phoenix;integrated security=sspi");
+        //private const string InsertRequest = "insert into Request(RequestId,RequestTitle,RequestDetail,Comments,RequestStatus,EditDttm) values('{0}','{1}','{2}','',{3},'{4}')";
+       
+
         protected void SendForApprovalButtonClick(object sender, EventArgs e)
         {
-            var requestId = RequestId.Text;
-            var requestTitle = RequestTitle.Text;
-            var requestDetails = RequestDetails.Text;
-            int requestStatus = 0;
-            if (requestId == "" || requestTitle == "" || requestDetails == "")
+
+            if (RequestId.Text == "" || RequestTitle.Text == "" || RequestDetails.Text == "")
             {
                 Response.Write("<script>alert('Please fill out all fields!');</script>");
             }
             else
             {
-                _sqlConnection.Open();
-
-                var sqlBaseBuilder = new StringBuilder(InsertRequest);
-                var sqlStr = string.Format(sqlBaseBuilder.ToString(), requestId, requestTitle, requestDetails, requestStatus, DateTime.Now);
-
-                SqlCommand myCmd = new SqlCommand(sqlStr, _sqlConnection);
-                myCmd.ExecuteNonQuery();
-                myCmd.Dispose();
-                _sqlConnection.Close();
-
+                var addRequestResult = RequestModel.AddRequest(new Request{
+                    RequestId = RequestId.Text,
+                    RequestTitle= RequestTitle.Text,
+                    RequestDetail= RequestDetails.Text,
+                    RequestStatus=0,
+                    EditTime= DateTime.Now
+                });
+                if (addRequestResult)
+                {
+                    Response.Write("<script>alert('Add a new request successfully!');</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Add a new request failed!');</script>");
+                }
                 Response.Redirect("index.aspx");
             }
         }
 
         protected void NotifyButtonClick(object sender, EventArgs e)
         {
-            var requestId = RequestId.Text;
-            var requestTitle = RequestTitle.Text;
-            var requestDetails = RequestDetails.Text;
-            int requestStatus = 3;
-            if (requestId == "" || requestTitle == "" || requestDetails == "")
+            if (RequestId.Text == "" || RequestTitle.Text == "" || RequestDetails.Text == "")
             {
                 Response.Write("<script>alert('Please fill out all fields!');</script>");
             }
             else
             {
-                _sqlConnection.Open();
-
-                var sqlBaseBuilder = new StringBuilder(InsertRequest);
-                var sqlStr = string.Format(sqlBaseBuilder.ToString(), requestId, requestTitle, requestDetails, requestStatus, DateTime.Now);
-
-                SqlCommand myCmd = new SqlCommand(sqlStr, _sqlConnection);
-                myCmd.ExecuteNonQuery();
-                myCmd.Dispose();
-                _sqlConnection.Close();
-
+                var addRequestResult = RequestModel.AddRequest(new Request
+                {
+                    RequestId = RequestId.Text,
+                    RequestTitle = RequestTitle.Text,
+                    RequestDetail = RequestDetails.Text,
+                    RequestStatus = 3,
+                    EditTime = DateTime.Now
+                });
+                if (addRequestResult)
+                {
+                    Response.Write("<script>alert('Add a new request successfully!');</script>");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Add a new request failed!');</script>");
+                }
                 Response.Redirect("index.aspx");
             }
         }
