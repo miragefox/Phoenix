f155707860ad0de7364fd07de7151f66b4dc87f5
EmmaCat <li.f.liu@accentue.com>
2017-12-26 18:35:24 +0800

    update

--- Web Application/Phoenix/Phoenix/request.aspx.cs
+++ Web Application/Phoenix/Phoenix/request.aspx.cs
@@ -3,17 +3,20 @@ using System.Data.SqlClient;
 using System;
 using System.Text;
 using Phoenix.PhoenixDataModel;
+using static Phoenix.RequestStatus;
 
 namespace Phoenix
 {
     public partial class request : System.Web.UI.Page
     {
         RequestModel requestmodel = new RequestModel();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             var requestid = Guid.NewGuid().ToString();
             RequestId.Text = requestid;
         }
+
         protected void SendForApprovalButtonClick(object sender, EventArgs e)
         {
 
@@ -23,21 +26,8 @@ namespace Phoenix
             }
             else
             {
-                bool addRequestResult = requestmodel.AddRequest(new Request{
-                    RequestId = RequestId.Text,
-                    RequestTitle= RequestTitle.Text,
-                    RequestDetail= RequestDetails.Text,
-                    RequestStatus=0,
-                    EditTime= DateTime.Now
-                });
-                if (addRequestResult)
-                {
-                    Response.Write("<script>alert('Add a new request successfully!');</script>");
-                }
-                else
-                {
-                    Response.Write("<script>alert('Add a new request failed!');</script>");
-                }
+                bool addRequestFeedback = SaveNewRequest(RequestStatusDetail.PENDINGREVIEW);
+                ReturnFeedback(addRequestFeedback);
                 Response.Redirect("index.aspx");
             }
         }
@@ -49,28 +39,39 @@ namespace Phoenix
             }
             else
             {
-                bool addRequestResult = requestmodel.AddRequest(new Request
-                {
-                    RequestId = RequestId.Text,
-                    RequestTitle = RequestTitle.Text,
-                    RequestDetail = RequestDetails.Text,
-                    RequestStatus = 3,
-                    EditTime = DateTime.Now
-                });
-                if (addRequestResult)
-                {
-                    Response.Write("<script>alert('Add a new request successfully!');</script>");
-                }
-                else
-                {
-                    Response.Write("<script>alert('Add a new request failed!');</script>");
-                }
+                bool addRequestFeedback = SaveNewRequest(RequestStatusDetail.COMPLETED);
+                ReturnFeedback(addRequestFeedback);
                 Response.Redirect("index.aspx");
             }
         }
         protected void CancelButtonClick(object sender, EventArgs e)
         {
             Response.Redirect("index.aspx");
+        }
+
+        public bool SaveNewRequest(RequestStatusDetail requestStatus)
+        {
+            bool addRequestFeedback = requestmodel.AddRequest(new Request
+            {
+                RequestId = RequestId.Text,
+                RequestTitle = RequestTitle.Text,
+                RequestDetail = RequestDetails.Text,
+                RequestStatus = requestStatus,
+                EditTime = DateTime.Now
+            });
+            return addRequestFeedback;
+        }
+
+        public void ReturnFeedback(bool feedback)
+        {
+            if (feedback)
+            {
+                Response.Write("<script>alert('Add a new request successfully!');</script>");
+            }
+            else
+            {
+                Response.Write("<script>alert('Add a new request failed!');</script>");
+            }
         }
     }
 }
