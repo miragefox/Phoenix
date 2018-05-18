273fae85453cf57ba163df34ff465410a58fa71d
Zhang <lei.l.zhang@accenture.com>
2018-05-18 15:35:07 +0800

    1
    1

---
+++ Web Application/Phoenix/Phoenix/Event.Master.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Phoenix
+{
+    public partial class Event : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("EventList.aspx");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("EventPendingList.aspx");
+        }
+    }
+}
